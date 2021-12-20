e15a9a81ba681bc5982b02f0519b0a8c42ed746b
fmixson <58919440+fmixson@users.noreply.github.com>
2021-12-20 07:48:44 -0800

    initial commit2

--- main.py
+++ main.py
@@ -25,6 +25,16 @@ for student_id in student_id_list:
                    major1='Core', major1_units=15, major1_disciplines=1, major1_no_of_courses=5,
                    major2='ListA', major2_units=4, major2_disciplines=1, major2_no_of_courses=1,
                    major3='ListB', major3_units=6, major3_disciplines=1, major3_no_of_courses=2)
+    # sorting_PlanB_majors(enrollment_history=enrollment_history, major_name="English for Transfer-AAT",
+    #                      major_course_requirements='AAT_English.csv',
+    #                      major1='Core', major1_units=3, major1_disciplines=1,
+    #                      major2='ListA', major2_units=6, major2_disciplines=1,
+    #                      major3='ListB', major3_units=6, major3_disciplines=1,
+    #                      major4='ListC', major4_units=3, major4_disciplines=1)
+    # sorting_PlanB_majors(enrollment_history=enrollment_history, major_name="Spanish for Transfer-AAT",
+    #                      major_course_requirements='AAT_Spanish.csv',
+    #                      major1='Core', major1_units=19, major1_disciplines=1,
+    #                      major2='ListA', major2_units=3, major2_disciplines=1)
 
 # DegreeCompletionReport.LS_AA_Degrees_df.sort_values(by=['Total_Missing'], inplace=True, ascending=True)
 DegreeCompletionReport.LS_AA_Degrees_df.to_csv(
